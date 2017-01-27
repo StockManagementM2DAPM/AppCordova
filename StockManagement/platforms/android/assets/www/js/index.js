d0dda7bb6a49d67475219f89d16414b7222bddb7
stephanegrasselli <stephane.grasselli@gmail.com>
2017-01-27 02:22:47 +0100

    adds REST request skeleteon

--- StockManagement/platforms/android/assets/www/js/index.js
+++ StockManagement/platforms/android/assets/www/js/index.js
@@ -16,18 +16,10 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- 
-$(function(){
-    // On initialise le header
-     $( ":jqmData(role='header')" ).toolbar({ defaults: true });
-     $(document).on("pagecontainerchange",function(){	
-                                 app.onDeviceReady();
-                 });
 
-     app.initalize();
-});
- 
- 
+var MATERIAL = null;
+var ROOM = null;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -40,35 +32,91 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        $( ":jqmData(role='header')" ).toolbar({ defaults: true });
+        $(document).on("pagecontainerchange",function(){   
+            if($.mobile.activePage.attr("id") == "rooms"){
+                console.log("rooms");
+            }
+            else if($.mobile.activePage.attr("id") == "materials"){
+                    console.log("materials");
+                    getAll();
+            }
+            else if($.mobile.activePage.attr("id") == "scan"){
+                    console.log("scan");
+                    $("#btn_Scan").click(startScan)
+            }
+        });
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-
-        if($.mobile.activePage.attr("id") == "rooms"){
-                console.log("rooms");
-        }
-        else if($.mobile.activePage.attr("id") == "materials"){
-                console.log("materials");
-        }
-        else if($.mobile.activePage.attr("id") == "scan"){
-                console.log("scan");
-                $("#btn_Scan").click(startScan)
-        }
-        console.log('Received Event: ' + id +'on '+$.mobile.activePage.attr("id"));
+        console.log('Received Event: ' + id);
     }
 };
 
+app.initialize();
+
+/*
+ * opens scanner for QR-Codes
+ * params : NO
+ * return : NO
+ */
+
 function startScan(){
     cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
+        function (result) {
+            alert("We got a barcode\n" +
                 "Result: " + result.text + "\n" +
                 "Format: " + result.format + "\n" +
                 "Cancelled: " + result.cancelled);
-      }, 
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
+            sendMovement(result.text);
+        }, 
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
    );
 }
+
+
+/*
+ * 
+ * params : 
+ * return :
+ */
+
+function sendMovement(param){
+    if(MATERIAL == null){
+        MATERIAL = param;
+        alert("Le matériel "+MATERIAL+" a bien été scanné.")
+    }else{
+        $.ajax('http://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PUT',
+            data: {
+                id: 1,
+                title: 'foo'+MATERIAL,
+                body: 'bar'+param,
+                userId: 1
+            }
+        }).then(function(data) {
+            console.log(data);
+        });
+        MATERIAL = null;
+    }
+}
+
+function getAll(){
+    $.ajax('http://jsonplaceholder.typicode.com/posts', {
+        method: 'GET'
+    }).then(function(data) {
+        console.log(data);
+        var content = '<ul id="listv" style="text-transform:uppercase;" data-role="listview">';
+        data.forEach(function(obj){
+            content += '<li>'+obj+'</li>';
+        });
+        content+='</ul>';
+        $("#materials_content").html(content);
+        $("#listv").listview().listview("refresh");
+    });
+}
+
+
